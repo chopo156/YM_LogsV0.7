086c6d33adc179d24a39e3a013ee3c92d7c51004
yrahmial17 <61751600+yrahmial17@users.noreply.github.com>
2020-05-25 13:00:05 +0300

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,76 @@
+----------------------------
+-- Coded By Yrahmial#7579 --
+----------------------------
+
+local YM_WebHook = 'https://discordapp.com/api/webhooks/713970556629811230/zIB5n9G66leV-AWSJQLqfSFoSBPOKe8Etjl-dlenXSFPfl5p-A7mkutmj5QznCHp2QWb'
+local YM_image = 'https://cdn.discordapp.com/attachments/713810142654627920/714112730285736027/fdg.png'
+--local LogsName = 'YM_Logs | By Yrahmial'
+local ym = message
+local time = os.date("%Y/%m/%d %X")
+
+
+
+AddEventHandler('playerConnecting', function() 
+    sendToDiscord('[Player Connecting] \n' .. GetPlayerName(source) .. 'Connect to The Server.')
+end)
+
+
+
+AddEventHandler('playerDropped', function(reason) 
+    sendToDiscord('[Player Logout] \n' .. GetPlayerName(source) .. 'Out From The Server. \n **Reason: ' .. reason)
+end)
+
+
+
+AddEventHandler('chatMessage', function(source, playername, ym)
+    sendToDiscord(playername, ym)
+    PerformHttpRequest(YM_WebHook, function(err, text, headers) end, 'POST', json.encode({username = GetPlayerName(source), embeds = connect, avatar_url = YM_image}), { ['Content-Type'] = 'application/json' })
+end)
+
+
+
+RegisterServerEvent('playerDied')
+AddEventHandler('playerDied',function(ym)
+    sendToDiscord('[Death log]', ym)
+end)
+
+
+
+--[[AddEventHandler('playerSpawned', function(source)
+    sendToDiscord('Player Spawned \n\n **Spawned Successfully**')
+end)]]--
+
+
+
+AddEventHandler('onServerResourceStart', function(resource)
+    if GetCurrentResourceName() == resource then
+        sendToDiscord ('[Server Status] \n\n On \n\n')
+        PerformHttpRequest(YM_WebHook, function(err, text, headers) end, 'POST', json.encode({username = 'YM_Logs | By Yrahmial', embeds = connect, avatar_url = YM_image}), { ['Content-Type'] = 'application/json' })
+    end
+end)
+
+
+
+AddEventHandler('onServerResourceStop', function(resource)
+    if GetCurrentResourceName() == resource then
+        sendToDiscord ('[Server Status] \n\n off \n\n')
+        PerformHttpRequest(YM_WebHook, function(err, text, headers) end, 'POST', json.encode({username = 'YM_Logs | By Yrahmial', embeds = connect, avatar_url = YM_image}), { ['Content-Type'] = 'application/json' })
+    end
+end)
+
+
+
+function sendToDiscord(name, ym, color)
+  local connect = {
+        {
+            ['color'] = 0255196,
+            ['title'] = '**'.. name ..'**',
+            ['description'] = ym,
+            ['footer'] = {
+            --['text'] = LogsName,
+            ['text'] = time,
+            },
+        }
+    }
+  PerformHttpRequest(YM_WebHook, function(err, text, headers) end, 'POST', json.encode({username = 'YM_Logs | By Yrahmial', embeds = connect, avatar_url = YM_image}), { ['Content-Type'] = 'application/json' })
+end
