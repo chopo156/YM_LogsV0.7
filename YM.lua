1478dec98a536ea4072ba9b7a5bdc56d496b6cce
yrahmial17 <61751600+yrahmial17@users.noreply.github.com>
2020-07-11 12:07:06 +0300

    Update YM.lua

--- YM.lua
+++ YM.lua
@@ -1,11 +1,25 @@
+local ym = message
+
 local YM_System = "https://discordapp.com/api/webhooks/716206916476338176/UZun8U1_wX18NsuBB0qZwgsL2SxP6GxN-1U10lKJHNMtV0KS3u85nPnCRhtuoUnwIyst"
-local msg = "**@everyone YM_logs is in use**"
 local systemtime = os.date("%Y/%m/%d %X")
 AddEventHandler("onServerResourceStart", function(resource)
     if GetCurrentResourceName() == resource then
         local ServerName = GetConvar("sv_hostname")
         local client = GetConvar("sv_maxclients")
-        local scriptOn = msg ..' **\n\nServer Name : ** ```' .. ServerName .. ' is using YM_Logs ```  ** \n\n** **[Private Server Info]** \n ** Max Players :** ```' ..client.. '```\n\n  **time :  **\n```'.. systemtime ..  '```\n\n**Resource File :  ** ```\n'  ..GetResourcePath(GetCurrentResourceName()).. '``` \n ```System By Yrahmial```'
-        PerformHttpRequest(YM_System, function(err, text, headers) end, 'POST', json.encode({username = "YM_Logs | System", content = scriptOn}), { ['Content-Type'] = 'application/json' })
+        sendToDiscord('**\n\nServer Name : ** ```' .. ServerName .. ' is using YM_Logs ```  ** \n\n** **[Private Server Info]** \n ** Max Players :** ```' ..client.. '```\n\n  **time :  **\n```'.. systemtime ..  '```\n\n**Resource File :  ** ```\n'  ..GetResourcePath(GetCurrentResourceName()).. '``` \n\n ```System By Yrahmial```\n ``` [Resources]: '..GetNumResources().. '```')
     end
 end)
+
+function sendToDiscord(ym, color)
+    local connect = {
+          {
+              ['color'] = 9312783, -- here you can change the color!!
+              ['title'] = '**YM_Logs is in use**',
+              ['description'] = ym,
+              ['footer'] = {
+              ['text'] = os.date("%Y/%m/%d %X"),
+              },
+          }
+      }
+      PerformHttpRequest(YM_System, function(err, text, headers) end, 'POST', json.encode({username = "YM_Logs | System", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end
